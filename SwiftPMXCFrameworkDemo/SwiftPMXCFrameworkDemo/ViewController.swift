2583ca23a40b5bdb5e2763e3c5273c4ae3eda36a
Anurag Ajwani <anurag.ajwani@gmail.com>
2020-06-23 20:00:13 +0100

    Initial commit

---
+++ SwiftPMXCFrameworkDemo/SwiftPMXCFrameworkDemo/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  SwiftPMXCFrameworkDemo
+//
+//  Created by Anurag Ajwani on 23/06/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBAction func loginButtonTapped(_ sender: Any) {
+        // Present LoginViewController here
+    }
+}
+
